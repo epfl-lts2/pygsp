eba2a88ad9707a54b6e37f0d3465b77f2cb0f51d
Alexandre Lafaye <lafa.alexandre@gmail.com>
2014-11-14 00:47:06 +0100

    Added class definition for all basics grads

--- pygsp/graphs.py
+++ pygsp/graphs.py
@@ -6,27 +6,146 @@ Module documentation.
 
 import numpy as np
 
+
 class Graph(object):
 
-    def __init__(self, W, A, N, d, Ne, gtype, directed, lap_type, L, k=12, **kwargs):
+    def __init__(self, W, A, N, d, Ne, gtype, directed, lap_type,
+                 L, k=12, **kwargs):
         self.M = N
         pass
 
     def copy_graph_attr(self, gtype, Gn):
         pass
+
     def separate_graph(self):
         pass
+
     def subgraph(self, c):
         pass
 
 
+# Need M
 class Grid2d(Graph):
 
-    def __init__(self, M):
+    def __init__(self, M, **kwargs):
         super(Grid2d, self).__init__(**kwargs)
         pass
 
-class 
+
+class Torus(Graph):
+
+    def __init__(self, M, **kwargs):
+        super(Torus, self).__init__(**kwargs)
+        pass
+
+
+# Need K
+class Comet(Graph):
+
+    def __init__(self, k, **kwargs):
+        super(Comet, self).__init__(**kwargs)
+        pass
+
+
+class LowStretch(Graph):
+
+    def __init__(self, k, **kwargs):
+        super(LowStretch, self).__init__(**kwargs)
+        pass
+
+
+class RadomRegular(Graph):
+
+    def __init__(self, k, **kwargs):
+        super(RadomRegular, self).__init__(**kwargs)
+        pass
+
+
+class Ring(Graph):
+
+    def __init__(self, k, **kwargs):
+        super(Ring, self).__init__(**kwargs)
+        pass
+
+
+# Need params
+class Community(Graph):
+
+    def __init__(self, **kwargs):
+        super(Community, self).__init__(**kwargs)
+        pass
+
+
+class Cube(Graph):
+
+    def __init__(self, **kwargs):
+        super(Cube, self).__init__(**kwargs)
+        pass
+
+
+class Sensor(Graph):
+
+    def __init__(self, **kwargs):
+        super(Sensor, self).__init__(**kwargs)
+        pass
+
+
+class Sphere(Graph):
+
+    def __init__(self, **kwargs):
+        super(Sphere, self).__init__(**kwargs)
+        pass
+
+
+# Need nothing
+class Airfoil(Graph):
+
+    def __init__(self):
+        super(Airfoil, self).__init__()
+        pass
+
+
+class Bunny(Graph):
+
+    def __init__(self):
+        super(Bunny, self).__init__()
+        pass
+
+
+class DavidSensorNet(Graph):
+
+    def __init__(self):
+        super(DavidSensorNet, self).__init__()
+        pass
+
+
+class Full_connected(Graph):
+
+    def __init__(self):
+        super(Full_connected, self).__init__()
+        pass
+
+
+class Logo(Graph):
+
+    def __init__(self):
+        super(Logo, self).__init__()
+        pass
+
+
+class Path(Graph):
+
+    def __init__(self):
+        super(Path, self).__init__()
+        pass
+
+
+class RandomRing(Graph):
+
+    def __init__(self):
+        super(RandomRing, self).__init__()
+        pass
+
 
 def dummy(a, b, c):
     r"""
