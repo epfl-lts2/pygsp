df0e70849076c28a6bc7181d2f3c31908a6d93ce
Alexandre Lafaye <lafa.alexandre@gmail.com>
2014-12-23 13:47:44 +0100

    added basic filters template

---
+++ pygsp/filters.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+r"""
+Flters Doc
+"""
+
+import numpy as np
+
+
+class Filter(object):
+    r"""
+    TODO doc
+    """
+
+    def __init__(self):
+        pass
+
+
+def dummy(a, b, c):
+    r"""
+    Short description.
+
+    Long description.
+
+    Parameters
+    ----------
+    a : int
+        Description.
+    b : array_like
+        Description.
+    c : bool
+        Description.
+
+    Returns
+    -------
+    d : ndarray
+        Description.
+
+    Examples
+    --------
+    >>> import pygsp
+    >>> pygsp.module1.dummy(0, [1, 2, 3], True)
+    array([1, 2, 3])
+
+    """
+    return np.array(b)
