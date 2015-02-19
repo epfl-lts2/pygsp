ed4b675ef7cdd48180b613020100732e10411113
Basile Châtillon <basile.chatillon@epfl.ch>
2015-02-19 17:08:42 +0100

    Adding grad, grad_mat operator.

--- pygsp/operators.py
+++ pygsp/operators.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from scipy import sparse
 from pygsp import utils
 
 
@@ -12,7 +12,15 @@ def grad(G, s):
     Graph gradient
 
     """
-    pass
+    if hasattr(G, 'lap_type'):
+        if G.lap_type == 'combinatorial':
+            print('Not implemented yet. However ask Nathanael it is very easy')
+            break
+
+    D = grad_mat(G)
+    gr = D*float(s)
+
+    return gr
 
 
 def grad_mat(G):
@@ -20,7 +28,26 @@ def grad_mat(G):
     Gradient sparse matrix of the graph
 
     """
-    pass
+    if not hasattr(G, 'v_in'):
+        G = gsp_adj2vec(G)
+        print('To be more efficient you should run: G = adj2vec(G); before using this proximal operator.')
+
+    if hasattr(G, 'Diff'):
+        D = G.Diff
+
+    else:
+        n = G.Ne
+        Dc = np.ones((2*n))
+        Dv = np.ones((2*n))
+
+        Dr = np.concatenate((np.arange(n), np.arange(n)))
+        Dc[:n] = G.v_in
+        Dc[n:] = G.v_out
+        Dv[:n] = np.sqrt(G.weights)
+        Dv[n:] = -np.sqrt(G.weight)
+        D = sparse.csc_matrix((Dv, (Dr, Dc)), shape=(n, G.N))
+
+    return D
 
 
 @utils.graph_array_handler
@@ -47,4 +74,3 @@ def full_eigen(L):
 
     # Sort everything
     EV = np.sort(eigenvalues)
-
