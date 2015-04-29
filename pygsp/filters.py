fce2f56b3e6a66fa031b2c0ceda534f4739b1b27
Basile Châtillon <basile.chatillon@epfl.ch>
2015-04-29 14:18:54 +0200

    Fix Abspline Filter.

--- pygsp/filters.py
+++ pygsp/filters.py
@@ -289,27 +289,35 @@ class Abspline(Filter):
         super(Abspline, self).__init__(G, **kwargs)
 
         def kernel_abspline3(x, alpha, beta, t1, t2):
-            r = np.zeros(x.shape)
-
             M = np.array([[1, t1, t1**2, t1**3],
                           [1, t2, t2**2, t2**3],
-                          [0, 1, 2*t1, 3*t1],
-                          [0, 1, 2*t2, 3*t2]])
+                          [0, 1, 2*t1, 3*t1**2],
+                          [0, 1, 2*t2, 3*t2**2]])
+            v = np.array([1, 1, t1**(-alpha) * alpha * t1**(alpha-1),
+                          -beta*t2**(-(beta+1)) * t2**beta])
+            a = np.linalg.solve(M, v)
 
-            v = np.array([1, 1, t1**(-alpha * alpha * t1**(alpha-1)),
-                          -beta*t2**(-(beta+1) * t2**beta)])
-            M = np.invert(M)
-            a = M.dot(v)
+            r1 = x <= t1
+            r2 = (x >= t1)*(x < t2)
+            r3 = (x >= t2)
 
-            r1 = x.any() >= 0 and x <= t1
-            r2 = x.any() >= t1 and x < t2
-            r3 = x.any() >= t2
+            if isinstance(x, np.float64):
 
-            x2 = x[r2]
+                if r1:
+                    r = x[r1]**alpha * t1**(-alpha)
+                if r2:
+                    r = a[0] + a[1] * x + a[2] * x**2 + a[3] * x**3
+                if r3:
+                    r = x[r3]**(-beta) * t2**beta
 
-            r[r1] = x[r1] ** alpha * t1 ** (-alpha)
-            r[r2] = a[0] + a[1] * x2 + a[2] * x2 ** 2 + a[3] * x2 ** 3
-            r[r3] = x[r3] ** -beta * t2 ** (beta)
+            else:
+                r = np.zeros(x.shape)
+
+                x2 = x[r2]
+
+                r[r1] = x[r1]**alpha * t1**(-alpha)
+                r[r2] = a[0] + a[1] * x2 + a[2] * x2**2 + a[3] * x2**3
+                r[r3] = x[r3]**(-beta) * t2 **beta
 
             return r
 
@@ -325,15 +333,12 @@ class Abspline(Filter):
 
         lminfac = .4 * G.lmin
 
-        self.g = []
-        self.g.append(lambda x: 1.2 * exp(-1) * gl(x / lminfac))
-
+        self.g = [lambda x: 1.2 * exp(-1) * gl(x / lminfac)]
         for i in range(0, Nf-1):
             self.g.append(lambda x, ind=i: gb(self.t[ind] * x))
 
         f = lambda x: -gb(x)
-        xstar = scipy.optimize.minimize_scalar(f, method='Bounded',
-                                               bounds=(1, 2))
+        xstar = scipy.optimize.minimize_scalar(f, bounds=(1, 2), method='bounded')
         gamma_l = -f(xstar.x)
         lminfac = .6 * G.lmin
         self.g[0] = lambda x: gamma_l * gl(x / lminfac)
