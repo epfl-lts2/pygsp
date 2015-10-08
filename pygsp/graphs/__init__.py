dace3f13f8f75c9dde00fbf10a940220a70c51b2
Lionel Martin <lionel.martin@polylan.ch>
2015-10-08 14:04:58 +0200

    StochasticBlockModel

--- pygsp/graphs/__init__.py
+++ pygsp/graphs/__init__.py
@@ -41,7 +41,7 @@ def dummy(a, b, c):
     return np.array(b)
 
 
-__all__ = ['Graph', 'Airfoil', 'Comet', 'Community', 'DavidSensorNet', 'FullConnected', 'Grid2d', 'Logo', 'LowStretchTree', 'Minnesota', 'Path', 'RandomRing', 'RandomRegular', 'Ring', 'Sensor', 'SwissRoll', 'Torus', 'ErdosRenyi']
+__all__ = ['Graph', 'Airfoil', 'Comet', 'Community', 'DavidSensorNet', 'FullConnected', 'Grid2d', 'Logo', 'LowStretchTree', 'Minnesota', 'Path', 'RandomRing', 'RandomRegular', 'Ring', 'Sensor', 'SwissRoll', 'Torus', 'ErdosRenyi', 'StochasticBlockModel']
 
 
 # Automaticaly import all classes from subfiles defined in __all__
