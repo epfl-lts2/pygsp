e28cab629abb75a24fabdf4fc28007aaa7fe7bf4
Alexandre Lafaye <lafa.alexandre@gmail.com>
2016-09-06 13:52:07 +0200

    readded files deleted by changed name folder

---
+++ pygsp/pointcloud/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+r"""This module implements some PointClouds."""
+
+import importlib
+import sys
+
+__all__ = ['PointCloud']
+
+
+# Automaticaly import all classes from subfiles defined in __all__
+for class_to_import in __all__:
+    setattr(sys.modules[__name__], class_to_import, getattr(importlib.import_module('.' + class_to_import.lower(), 'pygsp.pointcloud'), class_to_import))
