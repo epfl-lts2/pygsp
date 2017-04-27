7629c2a13c554dc5f79bf2a4befac439df8a8b4a
Lionel Martin <lionel.martin@epfl.ch>
2017-04-27 16:58:56 +0200

    Update python version support

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
