ae78da565a9064689183ad6b1838acbdb79cdcb6
Michaël Defferrard <michael.defferrard@epfl.ch>
2017-08-09 20:58:57 +0200

    PyQt5 is required for Python newer than 3.5 too

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'pyopengl',
         'pyqtgraph<=0.10.1',
         'matplotlib==1.4.3' if sys.version_info.major == 3 and sys.version_info.minor < 4 else 'matplotlib',
-        'PyQt5' if sys.version_info.major == 3 and sys.version_info.minor == 5 else 'PySide',
+        'PyQt5' if sys.version_info >= (3, 5) else 'PySide',
 	'pyflann' if sys.version_info.major == 2 else 'pyflann3'],
     license="BSD",
     keywords='graph signal processing toolbox filters pointclouds',
