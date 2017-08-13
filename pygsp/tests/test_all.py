cc4b212c16bedcbbbc2b1461be7ede4a773d3608
Michaël Defferrard <michael.defferrard@epfl.ch>
2017-08-13 22:53:45 +0200

    tests: tutorials and docstrings in main
    Only the docstrings of the 6 python files in the pygsp root were tested!

--- pygsp/tests/test_all.py
+++ pygsp/tests/test_all.py
@@ -3,28 +3,41 @@
 
 """
 Test suite for the pygsp package.
+
 """
 
+import os
 import unittest
-from pygsp.tests import test_docstrings, test_tutorials
-from pygsp.tests import test_graphs, test_filters, test_utils, test_plotting
+import doctest
+
+from . import test_graphs, test_filters, test_utils, test_plotting
+
+
+def gen_recursive_file(root, ext):
+    for root, dirnames, filenames in os.walk(root):
+        for name in filenames:
+            if name.lower().endswith(ext):
+                yield os.path.join(root, name)
+
+
+def test_docstrings(root, ext):
+    files = list(gen_recursive_file(root, ext))
+    return doctest.DocFileSuite(*files, module_relative=False)
 
 
 suites = []
-
-suites.append(test_docstrings.suite)
-suites.append(test_tutorials.suite)
 suites.append(test_graphs.suite)
 suites.append(test_utils.suite)
 suites.append(test_filters.suite)
 suites.append(test_plotting.suite)
-
+suites.append(test_docstrings('pygsp', '.py'))
+suites.append(test_docstrings('.', '.rst'))
 suite = unittest.TestSuite(suites)
 
 
-def run():
+def run():  # pragma: no cover
     unittest.TextTestRunner(verbosity=2).run(suite)
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # pragma: no cover
     run()
