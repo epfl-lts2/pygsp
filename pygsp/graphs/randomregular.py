7c26764943321baea94cb8331e9c56154d0d5b81
Michaël Defferrard <michael.defferrard@epfl.ch>
2017-08-14 11:52:06 +0200

    random regular graph: move construction in init

--- pygsp/graphs/randomregular.py
+++ pygsp/graphs/randomregular.py
@@ -11,17 +11,31 @@ from math import floor
 
 class RandomRegular(Graph):
     r"""
-    Create a random regular graphs
+    Create a random regular graph.
 
-    The random regular graph has the property that every nodes is connected to
-    'k' other nodes.
+    The random regular graph has the property that every node is connected to
+    k other nodes. That graph is simple (without loops or double edges),
+    k-regular (each vertex is adjacent to k nodes), and undirected.
 
     Parameters
     ----------
     N : int
         Number of nodes (default is 64)
     k : int
-        Number of connections of each nodes (default is 6)
+        Number of connections, or degree, of each node (default is 6)
+    maxIter : int
+        Maximum number of iterations (default is 10)
+
+    Notes
+    -----
+    The *pairing model* algorithm works as follows. First create n*d *half
+    edges*. Then repeat as long as possible: pick a pair of half edges and if
+    it's legal (doesn't create a loop nor a double edge) add it to the graph.
+
+    References
+    ----------
+    See :cite:`kim2003randomregulargraphs`.
+    This code has been adapted from matlab to python.
 
     Examples
     --------
@@ -30,22 +44,72 @@ class RandomRegular(Graph):
 
     """
 
-    def isRegularGraph(self, A):
+    def __init__(self, N=64, k=6, maxIter=10, **kwargs):
+        self.k = k
+
+        self.logger = build_logger(__name__, **kwargs)
+
+        # continue until a proper graph is formed
+        if (N * k) % 2 == 1:
+            raise ValueError("input error: N*d must be even!")
+
+        # a list of open half-edges
+        U = np.kron(np.ones(k), np.arange(N))
+
+        # the graphs adjacency matrix
+        A = sparse.lil_matrix(np.zeros((N, N)))
+
+        edgesTested = 0
+        repetition = 1
+
+        # check that there are no loops nor parallel edges
+        while np.size(U) and repetition < maxIter:
+            edgesTested += 1
+
+            # print(progess)
+            if edgesTested % 5000 == 0:
+                self.logger.debug("createRandRegGraph() progress: edges= "
+                                  "{}/{}.".format(edgesTested, n*k/2))
+
+            # chose at random 2 half edges
+            i1 = floor(rd.random()*np.shape(U)[0])
+            i2 = floor(rd.random()*np.shape(U)[0])
+            v1 = U[i1]
+            v2 = U[i2]
+
+            # check that there are no loops nor parallel edges
+            if v1 == v2 or A[v1, v2] == 1:
+                # restart process if needed
+                if edgesTested == N*k:
+                    repetition = repetition + 1
+                    edgesTested = 0
+                    U = np.kron(np.ones(k), np.arange(N))
+                    A = sparse.lil_matrix(np.zeros((N, N)))
+            else:
+                # add edge to graph
+                A[v1, v2] = 1
+                A[v2, v1] = 1
+
+                # remove used half-edges
+                v = sorted([i1, i2])
+                U = np.concatenate((U[:v[0]], U[v[0] + 1:v[1]], U[v[1] + 1:]))
+
+        super(RandomRegular, self).__init__(W=A, gtype="random_regular",
+                                            **kwargs)
+
+        self.is_regular()
+
+    def is_regular(self):
         r"""
         Troubleshoot a given regular graph.
-
-        Parameters
-        ----------
-        A : sparse matrix
 
         """
         warn = False
         msg = 'The given matrix'
 
         # check if the sparse matrix is in a good format
-        if A.getformat() == 'lil' or \
-                A.getformat() == 'dia' or \
-                A.getformat() == 'bok':
+        A = self.A
+        if A.getformat() in ['lil', 'dia', 'bok']:
             A = A.tocsc()
 
         # check symmetry
@@ -74,101 +138,3 @@ class RandomRegular(Graph):
             self.logger.warning('{}.'.format(msg[:-1]))
         else:
             self.logger.info('{} is ok.'.format(msg))
-
-    def createRandRegGraph(self, vertNum, deg, maxIter=10):
-        r"""
-        Create a simple d-regular undirected graph.
-
-        simple = without loops or double edges
-        d-regular = each vertex is adjacent to d edges
-
-        Parameters
-        ----------
-        vertNum : int
-            Number of vertices
-        deg : int
-            The degree of each vertex
-        maxIter : int
-            The maximum number of iterations
-
-        Returns
-        -------
-        A : sparse
-            Representation of the graph
-
-        Algorithm
-        ---------
-        "The pairing model": create n*d 'half edges'.
-        Repeat as long as possible: pick a pair of half edges
-        and if it's legal (doesn't create a loop nor a double edge)
-        add it to the graph
-
-        References
-        ----------
-        http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.67.7957&rep=rep1&type=pdf
-        (This code has been adapted from matlab to python)
-
-        """
-        n = vertNum
-        d = deg
-
-        # continue until a proper graph is formed
-        if (n * d) % 2 == 1:
-            raise ValueError("createRandRegGraph input err:\
-                                n*d must be even!")
-
-        # a list of open half-edges
-        U = np.kron(np.ones((d)), np.arange(n))
-
-        # the graphs adjacency matrix
-        A = sparse.lil_matrix(np.zeros((n, n)))
-
-        edgesTested = 0
-        repetition = 1
-
-        # check that there are no loops nor parallel edges
-        while np.size(U) and repetition < maxIter:
-            edgesTested += 1
-
-            # print(progess)
-            if edgesTested % 5000 == 0:
-                self.logger.debug("createRandRegGraph() progress: edges= "
-                                  "{}/{}.".format(edgesTested, n*d/2))
-
-            # chose at random 2 half edges
-            i1 = floor(rd.random()*np.shape(U)[0])
-            i2 = floor(rd.random()*np.shape(U)[0])
-            v1 = U[i1]
-            v2 = U[i2]
-
-            # check that there are no loops nor parallel edges
-            if v1 == v2 or A[v1, v2] == 1:
-                # restart process if needed
-                if edgesTested == n*d:
-                    repetition = repetition + 1
-                    edgesTested = 0
-                    U = np.kron(np.ones((d)), np.arange(n))
-                    A = sparse.lil_matrix(np.zeros((n, n)))
-            else:
-                # add edge to graph
-                A[v1, v2] = 1
-                A[v2, v1] = 1
-
-                # remove used half-edges
-                v = sorted([i1, i2])
-                U = np.concatenate((U[:v[0]], U[v[0] + 1:v[1]], U[v[1] + 1:]))
-
-        self.isRegularGraph(A)
-
-        return A
-
-    def __init__(self, N=64, k=6, **kwargs):
-        self.k = k
-
-        # Build the logger as createRandRegGraph need it
-        self.logger = build_logger(__name__, **kwargs)
-
-        W = self.createRandRegGraph(N, k)
-
-        super(RandomRegular, self).__init__(W=W, gtype="random_regular",
-                                            **kwargs)
