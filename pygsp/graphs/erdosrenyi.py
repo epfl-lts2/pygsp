33816873c3c6f82d1585e43528b4c495c99e0bbe
Michaël Defferrard <michael.defferrard@epfl.ch>
2017-09-19 20:04:39 +0200

    ErdosRenyi: error if the graph cannot be connected

--- pygsp/graphs/erdosrenyi.py
+++ pygsp/graphs/erdosrenyi.py
@@ -9,22 +9,22 @@ from . import Graph  # prevent circular import in Python < 3.5
 class ErdosRenyi(Graph):
     r"""Erdos Renyi graph.
 
-    The Erdos Renyi graph is constructed by connecting nodes randomly. Each
-    edge is included in the graph with probability p independent from every
+    The Erdos Renyi graph is constructed by randomly connecting nodes. Each
+    edge is included in the graph with probability p, independently from any
     other edge. All edge weights are equal to 1.
 
     Parameters
     ----------
     N : int
-        Number of nodes (default is 100)
+        Number of nodes (default is 100).
     p : float
-        Probability of connection of a node with another
+        Probability to connect a node with another one.
     connected : bool
         Force the graph to be connected (default is False).
     directed : bool
         Define if the graph is directed (default is False).
     max_iter : int
-        Maximum number of try to connect the graph (default is 10).
+        Maximum number of trials to connect the graph (default is 10).
 
     Examples
     --------
@@ -42,7 +42,8 @@ class ErdosRenyi(Graph):
         M = int(N * (N-1) if directed else N * (N-1) / 2)
         nb_elem = int(p * M)
 
-        for i in range(max_iter):
+        nb_iter = 0
+        while True:
             indices = np.random.permutation(M)[:nb_elem]
 
             if directed:
@@ -58,5 +59,11 @@ class ErdosRenyi(Graph):
 
             if not connected or self.is_connected(recompute=True):
                 break
+            nb_iter += 1
+            if nb_iter > max_iter:
+                raise ValueError('The graph could not be connected after {} '
+                                 'trials. Increase the connection probability '
+                                 'or the number of trials.'.format(max_iter))
 
-        super(ErdosRenyi, self).__init__(W=self.W, gtype=u"Erdös Renyi", **kwargs)
+        super(ErdosRenyi, self).__init__(W=self.W, gtype=u"Erdös Renyi",
+                                         **kwargs)
