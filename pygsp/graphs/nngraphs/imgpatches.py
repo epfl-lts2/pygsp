f4b8a2a34902172f9502c7c91b3becd3f8b98858
Michaël Defferrard <michael.defferrard@epfl.ch>
2017-12-13 15:45:15 +0100

    imgpatches: integrate utils.extract_patches (sole user)

--- pygsp/graphs/nngraphs/imgpatches.py
+++ pygsp/graphs/nngraphs/imgpatches.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from pygsp import utils
+import numpy as np
+import skimage
+
 from pygsp.graphs import NNGraph  # prevent circular import in Python < 3.5
 
 
 class ImgPatches(NNGraph):
     r"""NN-graph between patches of an image.
+
+    Extract a feature vector in the form of a patch for every pixel of an
+    image, then construct a nearest-neighbor graph between these feature
+    vectors. The feature matrix, i.e. the patches, can be found in :attr:`Xin`.
 
     Parameters
     ----------
@@ -17,12 +23,23 @@ class ImgPatches(NNGraph):
     kwargs : dict
         Parameters passed to :class:`NNGraph`.
 
+    Notes
+    -----
+    The feature vector of a pixel `i` will consist of the stacking of the
+    intensity values of all pixels in the patch centered at `i`, for all color
+    channels. So, if the input image has `d` color channels, the dimension of
+    the feature vector of each pixel is (patch_shape[0] * patch_shape[1] * d).
+
     Examples
     --------
     >>> import matplotlib.pyplot as plt
     >>> from skimage import data, img_as_float
     >>> img = img_as_float(data.camera()[::64, ::64])
-    >>> G = graphs.ImgPatches(img)
+    >>> G = graphs.ImgPatches(img, patch_shape=(3, 3))
+    >>> print('{} nodes ({} x {} pixels)'.format(G.Xin.shape[0], *img.shape))
+    64 nodes (8 x 8 pixels)
+    >>> print('{} features per node'.format(G.Xin.shape[1]))
+    9 features per node
     >>> G.set_coordinates(kind='spring', seed=42)
     >>> fig, axes = plt.subplots(1, 2)
     >>> _ = axes[0].spy(G.W, markersize=2)
@@ -33,6 +50,39 @@ class ImgPatches(NNGraph):
     def __init__(self, img, patch_shape=(3, 3), **kwargs):
 
         self.img = img
-        X = utils.extract_patches(img, patch_shape=patch_shape)
 
-        super(ImgPatches, self).__init__(X, gtype='patch-graph', **kwargs)
+        try:
+            h, w, d = img.shape
+        except ValueError:
+            try:
+                h, w = img.shape
+                d = 0
+            except ValueError:
+                print("Image should be at least a 2D array.")
+
+        try:
+            r, c = patch_shape
+        except ValueError:
+            r = patch_shape[0]
+            c = r
+
+        pad_width = [(int((r - 0.5) / 2.), int((r + 0.5) / 2.)),
+                     (int((c - 0.5) / 2.), int((c + 0.5) / 2.))]
+
+        if d == 0:
+            window_shape = (r, c)
+            d = 1  # For the reshape in the return call
+        else:
+            pad_width += [(0, 0)]
+            window_shape = (r, c, d)
+
+        # Pad the image.
+        img = np.pad(img, pad_width=pad_width, mode='symmetric')
+
+        # Extract patches as node features.
+        patches = skimage.util.view_as_windows(img, window_shape=window_shape)
+        patches = patches.reshape((h * w, r * c * d))
+
+        super(ImgPatches, self).__init__(patches,
+                                         gtype='patch-graph',
+                                         **kwargs)
--- pygsp/utils.py
+++ pygsp/utils.py
@@ -17,7 +17,6 @@ import io
 import numpy as np
 from scipy import sparse
 import scipy.io
-import skimage
 
 
 def build_logger(name):
@@ -400,72 +399,6 @@ def repmatline(A, ncol=1, nrow=1):
                          'equal to one, or you will get an empty array.')
 
     return np.repeat(np.repeat(A, ncol, axis=1), nrow, axis=0)
-
-
-def extract_patches(img, patch_shape=(3, 3)):
-    r"""
-    Extract a patch feature vector for every pixel of an image.
-
-    Parameters
-    ----------
-    img : array
-        Input image.
-    patch_shape : tuple, optional
-        Dimensions of the patch window. Syntax: (height, width), or (height,),
-        in which case width = height.
-
-    Returns
-    -------
-    array
-        Feature matrix.
-
-    Notes
-    -----
-    The feature vector of a pixel `i` will consist of the stacking of the
-    intensity values of all pixels in the patch centered at `i`, for all color
-    channels. So, if the input image has `d` color channels, the dimension of
-    the feature vector of each pixel is (patch_shape[0] * patch_shape[1] * d).
-
-    Examples
-    --------
-    >>> from pygsp import utils
-    >>> import skimage
-    >>> img = skimage.img_as_float(skimage.data.camera()[::2, ::2])
-    >>> X = utils.extract_patches(img)
-
-    """
-
-    try:
-        h, w, d = img.shape
-    except ValueError:
-        try:
-            h, w = img.shape
-            d = 0
-        except ValueError:
-            print("Image should be at least a 2-d array.")
-
-    try:
-        r, c = patch_shape
-    except ValueError:
-        r = patch_shape[0]
-        c = r
-    if d == 0:
-        pad_width = ((int((r - 0.5) / 2.), int((r + 0.5) / 2.)),
-                     (int((c - 0.5) / 2.), int((c + 0.5) / 2.)))
-        window_shape = (r, c)
-        d = 1  # For the reshape in the return call
-    else:
-        pad_width = ((int((r - 0.5) / 2.), int((r + 0.5) / 2.)),
-                     (int((c - 0.5) / 2.), int((c + 0.5) / 2.)),
-                     (0, 0))
-        window_shape = (r, c, d)
-    # Pad the image
-    img_pad = np.pad(img, pad_width=pad_width, mode='symmetric')
-
-    # Extract patches
-    patches = skimage.util.view_as_windows(img_pad, window_shape=window_shape)
-
-    return patches.reshape((h * w, r * c * d))
 
 
 def import_modules(names, src, dst):
