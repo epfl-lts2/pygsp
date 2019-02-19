ee985acc32841c439066e086655d31c885c4f9fa
Michaël Defferrard <m.deff@hotmail.com>
2019-02-19 22:14:53 +0100

    Update pygsp/graphs/graph.py
    Accept change
    
    Co-Authored-By: cgallay <charles.gallay@epfl.ch>

--- pygsp/graphs/graph.py
+++ pygsp/graphs/graph.py
@@ -178,7 +178,7 @@ class Graph(FourierMixIn, DifferenceMixIn, IOMixIn, LayoutMixIn):
 
         The weight are stored as an edge attribute under named `weight`
         The signals are stored as node attributes under the same name as define in PyGSP
-        :func:`~pygsp.graphs.Graph.set_signal`.
+        adding them with :meth:`set_signal`.
 
         Returns
         -------
