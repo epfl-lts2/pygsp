caf3cd143c98d184cd28475b89f8984e91376e3a
Michaël Defferrard <m.deff@hotmail.com>
2019-02-19 22:16:40 +0100

    Update pygsp/graphs/graph.py
    Accept change
    
    Co-Authored-By: cgallay <charles.gallay@epfl.ch>

--- pygsp/graphs/graph.py
+++ pygsp/graphs/graph.py
@@ -177,7 +177,7 @@ class Graph(FourierMixIn, DifferenceMixIn, IOMixIn, LayoutMixIn):
         r"""Export the graph to an `Networkx <https://networkx.github.io>`_ object
 
         The weight are stored as an edge attribute under named `weight`
-        The signals are stored as node attributes under the same name as define in PyGSP
+        The signals are stored as node attributes under the name given when
         adding them with :meth:`set_signal`.
 
         Returns
