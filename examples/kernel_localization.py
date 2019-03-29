1e8615cafd3501548471b911bab59d71a9b84032
Michaël Defferrard <michael.defferrard@epfl.ch>
2019-03-29 01:13:06 +0100

    doc: more examples from my talks

---
+++ examples/kernel_localization.py
@@ -0,0 +1,44 @@
+r"""
+Kernel localization
+===================
+
+In classical signal processing, a filter can be translated in the vertex
+domain. We cannot do that on graphs. Instead, we can
+:meth:`~pygsp.filters.Filter.localize` a filter kernel. Note how on classic
+structures (like the ring), the localized kernel is the same everywhere, while
+it changes when localized on irregular graphs.
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+import pygsp as pg
+
+#plt.rc('font', family='Latin Modern Roman')
+plt.rc('text', usetex=True)
+plt.rc('text.latex', preamble=r'\usepackage{lmodern}')
+
+fig, axes = plt.subplots(2, 4, figsize=(10, 4))
+
+graphs = [
+    pg.graphs.Ring(40),
+    pg.graphs.Sensor(64, seed=42),
+]
+
+locations = [0, 10, 20]
+
+for graph, axs in zip(graphs, axes):
+    graph.compute_fourier_basis()
+    g = pg.filters.Heat(graph)
+    g.plot(ax=axs[0], title='heat kernel')
+    axs[0].set_xlabel(r'eigenvalues $\lambda$')
+    axs[0].set_ylabel(r'$g(\lambda) = \exp \left( \frac{{-{}\lambda}}{{\lambda_{{max}}}} \right)$'.format(g.scale[0]))
+    maximum = 0
+    for loc in locations:
+        x = g.localize(loc)
+        maximum = np.maximum(maximum, x.max())
+    for loc, ax in zip(locations, axs[1:]):
+        graph.plot(g.localize(loc), limits=[0, maximum], highlight=loc, ax=ax,
+                   title=r'$g(L) \delta_{{{}}}$'.format(loc))
+        ax.set_axis_off()
+
+fig.tight_layout()
---
+++ examples/random_walk.py
@@ -0,0 +1,71 @@
+r"""
+Random walks
+============
+
+Probability of a random walker to be on any given vertex after a given number
+of steps starting from a given distribution.
+"""
+
+# sphinx_gallery_thumbnail_number = 2
+
+import numpy as np
+from scipy import sparse
+from matplotlib import pyplot as plt
+import pygsp as pg
+
+#plt.rc('font', family='Latin Modern Roman')
+plt.rc('text', usetex=True)
+plt.rc('text.latex', preamble=r'\usepackage{lmodern}')
+
+N = 7
+steps = [0, 1, 2, 3]
+
+graph = pg.graphs.Grid2d(N)
+delta = np.zeros(graph.N)
+delta[N//2*N + N//2] = 1
+
+probability = sparse.diags(graph.dw**(-1)) @ graph.W
+
+fig, axes = plt.subplots(1, len(steps), figsize=(12, 3))
+for step, ax in zip(steps, axes):
+    state = delta @ probability**step
+    graph.plot(state, ax=ax, title=r'$\delta P^{}$'.format(step))
+    ax.set_axis_off()
+
+fig.tight_layout()
+
+###############################################################################
+# Stationary distribution.
+
+graphs = [
+    pg.graphs.Ring(10),
+    pg.graphs.Grid2d(5),
+    pg.graphs.Comet(8, 4),
+    pg.graphs.BarabasiAlbert(20, seed=42),
+]
+
+fig, axes = plt.subplots(1, len(graphs), figsize=(12, 3))
+
+for graph, ax in zip(graphs, axes):
+
+    if not hasattr(graph, 'coords'):
+        graph.set_coordinates(seed=10)
+
+    P = sparse.diags(graph.dw**(-1)) @ graph.W
+
+#    e, u = np.linalg.eig(P.T.toarray())
+#    np.testing.assert_allclose(np.linalg.inv(u.T) @ np.diag(e) @ u.T,
+#                               P.toarray(), atol=1e-10)
+#    np.testing.assert_allclose(np.abs(e[0]), 1)
+#    stationary = np.abs(u.T[0])
+
+    e, u = sparse.linalg.eigs(P.T, k=1, which='LR')
+    np.testing.assert_allclose(e, 1)
+    stationary = np.abs(u).squeeze()
+    assert np.all(stationary < 0.71)
+
+    colorbar = False if type(graph) is pg.graphs.Ring else True
+    graph.plot(stationary, colorbar=colorbar, ax=ax, title='$xP = x$')
+    ax.set_axis_off()
+
+fig.tight_layout()
