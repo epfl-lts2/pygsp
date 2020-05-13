4459751aaaa9012383af31d8e669dd97e091c8b2
Michaël Defferrard <michael.defferrard@epfl.ch>
2020-05-13 04:00:54 +0200

    better tests for Comet and Path

--- pygsp/tests/test_graphs.py
+++ pygsp/tests/test_graphs.py
@@ -525,8 +525,28 @@ class TestCase(unittest.TestCase):
     def test_torus(self):
         graphs.Torus()
 
-    def test_comet(self):
-        graphs.Comet()
+    def test_comet(self, n=100, k=10):
+        graph = graphs.Comet(n, k)
+        self.assertEqual(graph.n_vertices, n)
+        self.assertEqual(graph.n_edges, n-1)
+        self.assertEqual(graph.dw[0], k)
+        graph = graphs.Comet(7, 4)
+        adjacency = [
+            [0, 1, 1, 1, 1, 0, 0],  # center
+            [1, 0, 0, 0, 0, 0, 0],  # branch
+            [1, 0, 0, 0, 0, 0, 0],  # branch
+            [1, 0, 0, 0, 0, 0, 0],  # branch
+            [1, 0, 0, 0, 0, 1, 0],  # tail
+            [0, 0, 0, 0, 1, 0, 1],  # tail
+            [0, 0, 0, 0, 0, 1, 0],  # tail
+        ]
+        np.testing.assert_array_equal(graph.W.toarray(), adjacency)
+        coords = [[0, 0], [0, 1], [-1, 0], [0, -1], [1, 0], [2, 0], [3, 0]]
+        np.testing.assert_allclose(graph.coords, coords, atol=1e-10)
+        # Comet generalizes Path.
+        g1 = graphs.Comet(n, 0)
+        g2 = graphs.Path(n)
+        np.testing.assert_array_equal(g1.W.toarray(), g2.W.toarray())
 
     def test_lowstretchtree(self):
         graphs.LowStretchTree()
@@ -592,8 +612,28 @@ class TestCase(unittest.TestCase):
     def test_logo(self):
         graphs.Logo()
 
-    def test_path(self):
-        graphs.Path()
+    def test_path(self, n=5):
+        graph = graphs.Path(n, directed=False)
+        adjacency = [
+            [0, 1, 0, 0, 0],
+            [1, 0, 1, 0, 0],
+            [0, 1, 0, 1, 0],
+            [0, 0, 1, 0, 1],
+            [0, 0, 0, 1, 0],
+        ]
+        coords = [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0]]
+        np.testing.assert_array_equal(graph.W.toarray(), adjacency)
+        np.testing.assert_array_equal(graph.coords, coords)
+        graph = graphs.Path(n, directed=True)
+        adjacency = [
+            [0, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0],
+        ]
+        np.testing.assert_array_equal(graph.W.toarray(), adjacency)
+        np.testing.assert_array_equal(graph.coords, coords)
 
     def test_randomring(self):
         graphs.RandomRing()
